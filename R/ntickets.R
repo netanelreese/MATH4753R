bc03f3b56738f843d33d2c24f84a2e0d7f62a75b
Nathanael <ngreese7@ou.edu>
2023-04-20 14:36:12 -0500

    Added some functions to use

--- R/ntickets.R
+++ R/ntickets.R
@@ -1,14 +1,14 @@
-
 #' ntickets
 #'
 #' @param N number of seats on a flight
 #' @param gamma probibility that a flight will be overbooked
 #' @param p probability for a "show"
 #'
-#' @returns list - a list containing nc the optimum number of tickets sold in a continuious manner, nd a distributed
 #'
-#' @examples
-#' ntickets(N = 200, gamma = .05,p = .95)
+#'
+#' @returns list - a list containing nc the optimum number of tickets sold in a continuious manner, nd a distributed
+#' @export
+#'
 ntickets = function(N, gamma,p){
   domain <- N:(N + round(0.1 * N))
   nd <- 1 - gamma - pbinom(q = N, size = domain, prob = p) # discrete distribution
@@ -24,14 +24,16 @@ ntickets = function(N, gamma,p){
 
 
 
-  plot(domain, nd, type = 'b', main=paste("Objective Vs n to find optimal tickets sold\n", "(", d, ")", "gamma = ", gamma, "N = ", N, "discrete"), ylab = "Objective", pch = 16, col = "blue")
+  plot(domain, nd, type = 'b', main=paste("Objective Vs n to find optimal tickets sold\n", "(", d, ")", "gamma = ", gamma, "N = ", N, "discrete"), ylab = "Objective", pch = 16)
   abline(h = 0, v = d, lwd = 2, col = "coral")
 
-  plot(domain, nc, type = 'l', main=paste("Objective Vs n to find optimal tickets sold\n", "(", domain[c], ")", "gamma = ", gamma, "N = ", N, "continuous"), ylab = "Objective", col = "red")
+  plot(domain, nc, type = 'l', main=paste("Objective Vs n to find optimal tickets sold\n", "(", domain[c], ")", "gamma = ", gamma, "N = ", N, "continuous"), ylab = "Objective")
   abline(h = 0, v = domain[c], col = "lavender", lwd = 2)
 
   list <- list(nd=d,nc=c, N=N, p=p, gamma=gamma)
   print(list)
-  return(list)
+  #return(list)
 }
-#ntickets(200, .05, .95)
+#0 errors ✔ | 0 warnings ✔ | 3 notes ✖
+
+#R CMD check succeeded
